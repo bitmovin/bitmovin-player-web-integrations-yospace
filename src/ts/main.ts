030b84b1da3f785182dcef11c9fd083349d5e4f3
David Steinacher <david.steinacher@bitmovin.com>
2018-11-05 15:21:56 +0100

    cleanup global export

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,8 +1,1 @@
-import { BitmovinYoSpacePlayer } from './YoSpaceAdManagement';
-
-// Export YoSpace Ad Management to global namespace
-let w = (window as any);
-w.bitmovin = w.bitmovin || {};
-w.bitmovin.player = w.bitmovin.player || {};
-w.bitmovin.player.analytics = w.bitmovin.player.analytics || {};
-w.bitmovin.player.analytics.ConvivaAnalytics = BitmovinYoSpacePlayer;
+export { BitmovinYoSpacePlayer } from './YoSpaceAdManagement';
