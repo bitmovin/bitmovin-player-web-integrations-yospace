dda059388afb69b8f497adc7e61f11e92866f993
David Steinacher <david.steinacher@bitmovin.com>
2019-01-10 12:59:36 +0100

    add missing spaces

--- src/ts/VastHelper.ts
+++ src/ts/VastHelper.ts
@@ -11,6 +11,6 @@ export class VastHelper {
 
   static buildVastXml(ad: VASTAd): string {
     // build a valid VAST xml data uri to schedule only the current vpaid ad
-    return 'data:text/xml,' + encodeURIComponent('<VAST version="3.0">'+ad.vastXML.outerHTML+'</VAST>');
+    return 'data:text/xml,' + encodeURIComponent('<VAST version="3.0">' + ad.vastXML.outerHTML + '</VAST>');
   }
 }
