fd7d621ab25a185d29af0c40ede61d406a176304
David Steinacher <david.steinacher@bitmovin.com>
2019-01-10 12:46:03 +0100

    rename VastExtensionHelper to VastHelper * add buildVastXml helper method

--- src/ts/VastHelper.ts
+++ src/ts/VastHelper.ts
@@ -1,11 +1,16 @@
 ///<reference path="Yospace.d.ts"/>
 import X2JS = require('x2js');
 
-export class VastExtensionHelper {
+export class VastHelper {
 
   static getExtensions(ad: VASTAd): any[] {
     return ad.Extensions.map((extension: XMLDocument) => {
       return new X2JS({ arrayAccessFormPaths: [/./g] }).dom2js(extension);
     });
+  }
+
+  static buildVastXml(ad: VASTAd): string {
+    // build a valid VAST xml data uri to schedule only the current vpaid ad
+    return 'data:text/xml,' + encodeURIComponent('<VAST version="3.0">'+ad.vastXML.outerHTML+'</VAST>');
   }
 }
