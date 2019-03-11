3b41d5387550e4a27b871aa00972df0199c6d508
David Steinacher <david.steinacher@bitmovin.com>
2019-03-11 12:47:55 +0100

    minor type changes due to npm package testing

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,8 @@
-export { BitmovinYospacePlayer, YospaceAssetType } from './YospaceAdManagement';
+export {
+  BitmovinYospacePlayer, YospaceAssetType, YospaceConfiguration, YospaceSourceConfig,
+} from './YospaceAdManagement';
+export { BitmovinYospacePlayerPolicy } from './BitmovinYospacePlayerPolicy';
+export {
+  YospaceErrorEvent, YospaceEventBase, YospacePlayerEvent, YospacePolicyErrorEvent, YospacePolicyErrorCode,
+  YospaceErrorCode,
+} from './YospaceError';
