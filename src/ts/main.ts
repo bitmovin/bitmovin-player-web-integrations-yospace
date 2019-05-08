5e4cdc2d01550e46bd79f37ba6499c9cff6af3ab
David Steinacher <david.steinacher@bitmovin.com>
2019-05-08 16:01:18 +0200

    move public API and interfaces to single file

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,6 @@
 export { BitmovinYospacePlayer } from './BitmovinYospacePlayer';
-export { YospaceAssetType, YospaceConfiguration, YospaceSourceConfig } from './InternalBitmovinYospacePlayer';
-export { BitmovinYospacePlayerPolicy } from './BitmovinYospacePlayerPolicy';
 export {
   YospaceErrorEvent, YospaceEventBase, YospacePlayerEvent, YospacePolicyErrorEvent, YospacePolicyErrorCode,
-  YospaceErrorCode,
-} from './YospaceError';
+  YospaceErrorCode, BitmovinYospacePlayerAPI, BitmovinYospacePlayerPolicy, YospaceAssetType, YospaceConfiguration,
+  YospaceSourceConfig, YospacePlayerType, BitmovinYospacePlayerExports, YospacePlayerEventCallback,
+} from './BitmovinYospacePlayerAPI';
