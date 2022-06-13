d94f4422429b8a7b3a546d86137356da14a3dc28
dweinber <daniel.weinberger@bitmovin.com>
2022-06-13 13:08:46 +0200

    Add eslint instead of tslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [{
+    files: ['src/**/*.ts'],
+    rules: {
+      // using rest parameters can lead to problems on ES5 targets
+      'prefer-rest-params': 'off',
+    }
+  }],
+};
