cbd943d9c3b2e6a709c1c4aa54eb16a48b168fd3
Kun Wu <kun.wu@eyevinn.se>
2022-11-18 11:40:28 +0100

    Update src/ts/InternalBitmovinYospacePlayer.ts
    Co-authored-by: Martin Stark <901824+martinstark@users.noreply.github.com>

--- src/ts/InternalBitmovinYospacePlayer.ts
+++ src/ts/InternalBitmovinYospacePlayer.ts
@@ -1084,7 +1084,6 @@ export class InternalBitmovinYospacePlayer implements BitmovinYospacePlayerAPI {
 
     // To support ads in live streams we need to track metadata events
     this.player.on(this.player.exports.PlayerEvent.Metadata, this.onMetaData);
-    this.player.on(this.player.exports.PlayerEvent.MetadataParsed, this.onMetaDataParsed);
   }
 
   private unregisterPlayerEvents(): void {
